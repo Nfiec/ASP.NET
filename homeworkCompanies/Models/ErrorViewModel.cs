a49e7aca124abb42ccb5614fe54e5206e458e255
Nfiec <95955366+Nfiec@users.noreply.github.com>
2023-02-27 20:00:41 +0600

    Add files via upload

---
+++ homeworkCompanies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace homeworkCompanies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
